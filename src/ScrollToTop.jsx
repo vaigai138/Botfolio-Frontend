fb26eb41a2d43dae592024e71c52d0cb067d5b6f
Vaigai Vendhan <133666759+vaigai138@users.noreply.github.com>
2025-08-07 10:12:26 +0530

    Update ScrollToTop.jsx

--- src/ScrollToTop.jsx
+++ src/ScrollToTop.jsx
@@ -3,11 +3,24 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    if (hash) {
+      const el = document.querySelector(hash);
+      if (el) {
+        setTimeout(() => {
+          if (window.lenis) {
+            window.lenis.scrollTo(el);
+          } else {
+            el.scrollIntoView({ behavior: "smooth" });
+          }
+        }, 100); // wait for DOM
+      }
+    } else {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, hash]);
 
   return null;
 };
