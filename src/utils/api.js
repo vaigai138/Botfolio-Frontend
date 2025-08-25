d8a6dc900caa2f0d7b21483531e337d703cd6706
Vaigai Vendhan <133666759+vaigai138@users.noreply.github.com>
2025-08-25 16:50:19 +0530

    Clarify token attachment in API requests

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,10 @@
-// api.js
 import axios from 'axios';
 
 const API = axios.create({
   baseURL: 'https://botfolio-cfefazfsadadh0gm.southeastasia-01.azurewebsites.net/api',
 });
 
-// Automatically attach token
+// Add token to each request
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
   if (token) req.headers.Authorization = `Bearer ${token}`;
