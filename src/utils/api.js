7fd0cf7c3804e973274d3f7dd8ab2bfa921a27fc
Vaigai Vendhan <133666759+vaigai138@users.noreply.github.com>
2025-08-25 16:33:15 +0530

    Automatically attach token to API requests
    Updated request interceptor to automatically attach token.

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,11 @@
+// api.js
 import axios from 'axios';
 
 const API = axios.create({
   baseURL: 'https://botfolio-cfefazfsadadh0gm.southeastasia-01.azurewebsites.net/api',
 });
 
-// Add token to each request
+// Automatically attach token
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
   if (token) req.headers.Authorization = `Bearer ${token}`;
